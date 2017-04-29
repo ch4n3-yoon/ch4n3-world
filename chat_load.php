0e59d4e2c1c021ebad685eed848052bd1f466f76
SeokChan-Yoon <chaneyoon@gmail.com>
2017-04-29 09:17:51 +0900

    Create chat_load.php

---
+++ chat_load.php
@@ -0,0 +1,27 @@
+<?php
+
+include('./dbconfig.php');
+// load chats from mysql DB
+$query = "SELECT * FROM (SELECT * FROM chat ORDER BY date DESC LIMIT 30) as t ORDER BY date DESC";
+$result = $mysqli->query($query);
+
+while ($rows = $result->fetch_array()) {  ?>
+
+    <li class="mdl-list__item">
+        <span class="mdl-list__item-primary-content">
+            <i class="material-icons mdl-list__item-icon"></i>
+            <?php
+
+            $date = $rows['date'];
+            $date = explode(" ", $date);
+            echo $date[0]; ?> | <?php echo $rows['nick'] ?> : <?php echo htmlentities($rows['text']);
+
+            ?>
+        </span>
+    </li>
+
+<?php
+
+}
+
+?>
