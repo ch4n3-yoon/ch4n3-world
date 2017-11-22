09dd0fac0af88afa980ecaf637b166bbdb24dba4
chaneyoon <chaneyoon@gmail.com>
2017-11-22 20:59:01 +0900

    채팅 관련 코드 개선

--- chat_load.php
+++ chat_load.php
@@ -1,31 +1,23 @@
-<?php
 
-/* create MySQL cursor */
-include('./dbconfig.php');
+    <?php
+    include('./dbconfig.php');
+    // load chats from mysql DB
+    $query = "SELECT * FROM (SELECT * FROM chat ORDER BY date DESC LIMIT 30) as t ORDER BY date DESC";
+    $result = $mysqli->query($query);
 
+    while ($rows = $result->fetch_array()) {
+        ?>
 
-/* load chat from Database*/
-$query = "SELECT * FROM (SELECT * FROM chat ORDER BY date DESC LIMIT 30) as t ORDER BY date DESC";
-$result = $mysqli->query($query);
+        <li class="mdl-list__item">
+            <span class="mdl-list__item-primary-content">
+                <i class="material-icons mdl-list__item-icon"></i>
+                 <?php
+                 $date = $rows['date'];
+        $date = explode(" ", $date);
+        echo $date[0]; ?> | <?php echo $rows['nick'] ?> : <?php echo htmlentities($rows['text']); ?>
+            </span>
+        </li>
 
+    <?php
 
-while ($rows = $result->fetch_array()) {  ?>
-
-    <li class="mdl-list__item">
-        <span class="mdl-list__item-primary-content">
-            <i class="material-icons mdl-list__item-icon"></i>
-            <?php
-
-            $date = $rows['date'];
-            $date = explode(" ", $date);
-            echo $date[0]; ?> | <?php echo $rows['nick'] ?> : <?php echo htmlentities($rows['text']);
-
-            ?>
-        </span>
-    </li>
-
-<?php
-
-}
-
-?>
+    } ?>
--- chat_send.php
+++ chat_send.php
@@ -1,5 +1,5 @@
-<!DOCTYPE html>
 <?php
+
 session_start();
 
 if(!isset($_SESSION['nick'])) {
@@ -9,24 +9,21 @@ if(!isset($_SESSION['nick'])) {
 include('./dbconfig.php');
 
 if(isset($_POST['text']) && isset($_POST['nick'])) {
-    if(preg_match("/\n|\t/i", $_POST['text'])) {
+    if(preg_match("/\)|\(|\'|\"|\\|\#|-|_|;|\n|\t/i", $_POST['text'])) {
         die("<script>alert('no hack ~_~'); location.href='./chat.php';</script>");
-    } else if(strlen($_POST['text'])>300) {
-        die("<script>alert('no more than 300'); location.href='./chat.php';</script>");
+    } else if(strlen($_POST['text'])>100) {
+        die("<script>alert('no more than 100'); location.href='./chat.php';</script>");
     } else if($_POST['text'] == " " && $_POST['text'] == "") {
         die("<script>alert('No whitespace only'); location.href='./chat.php';</script>");
     }
 
-    $_POST['text'] = preg_replace("/ㅅㅂ|시발|씨발|시1발|ㅆㅂ|스벌|쉬벌|ㅅㅂ/", "[안좋은말]", $_POST['text']);
+    $_POST['text'] = preg_replace("/ㅅㅂ|시발|씨발|시1발|ㅆㅂ/", "[안좋은말]", $_POST['text']);
     $_POST['text'] = preg_replace("/ㅄ|ㅂㅅ|병신|븅신|빙신|비융신/", "[나쁜말]",$_POST['text']);
     $_POST['text'] = preg_replace("/fuck|bitch|bullshit|damn|fucker/i", "[영어로하는나쁜말]",$_POST['text']);
-    $_POST['text'] = preg_replace("/윤석찬|석찬/", "[송중기, 공유보다 잘생긴 사람]",$_POST['text']);
-
-    $_POST['text'] = addslashes($_POST['text']);
+    $_POST['text'] = preg_replace("/윤석찬|석찬/", "송중기, 공유보다 잘생긴 사람",$_POST['text']);
 
     $query = "INSERT INTO chat(nick, text, date) VALUES('{$_SESSION['nick']}', ((((('{$_POST['text']}'))))), NOW())";
     $mysqli->query($query);
-    die("<script>location.href='./';</script>");
 }
 
 ?>
