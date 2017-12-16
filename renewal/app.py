f7320de38d2db41933c4aa81061fcf159589e2f9
ch4n3 <chaneyoon@gmail.com>
2017-12-17 03:40:45 +0900

    add render_template()

--- renewal/app.py
+++ renewal/app.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python
 # coding: utf-8
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 
 app = Flask(__name__)
+app.secret_key = "ch4n3isthesecretkey"
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    session['username'] = "ch4n3"
+    session['nickname'] = "admin123"
+    print session
+    return render_template("index.html", id=session['username'])
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
